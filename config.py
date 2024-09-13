203e8025411447e118f4fc9cee9ff91b95f484fb
jianbai-design <134767426+jianbai-design@users.noreply.github.com>
2024-09-13 20:34:06 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 # 公众号配置
 # 公众号appId
-app_id = "wx******************"
+app_id = "wxbc50988126d00b38"
 # 公众号appSecret
-app_secret = "f9a80260d33******************"
+app_secret = "305e1f80646ebc66e16ffbc1693dc66c"
 # 模板消息id
 # 每日消息
 template_id1 = "wX-************************************"
